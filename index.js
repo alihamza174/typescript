adb2a04efa77833c420d8840a582bcd8132aee00
shoaib-iqbal <shoaib.iqbal07@gmail.com>
2023-09-09 03:31:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// Question 2
+var mess = "Hello Eric, would you like to learn some Python today?";
+console.log(mess);
+// Question 3
+var person = "Ali Hamza";
+console.log(person.toLowerCase());
+console.log(person.toUpperCase());
+// Question 4
+var quote = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”';
+console.log(quote);
+// Question 5
+var famous_person = "Muhammad PBUH";
+var message = ' ${famous_person} is our beloved prophet';
+console.log(message);
+// Question 6
+var stripping_name = "                   Ali Hamza            ";
+console.log(stripping_name);
+console.log(stripping_name.trim());
+// Question 7
+var num_1 = 8;
+var num_2 = 4;
+console.log("Addition", num_1 + num_2);
+console.log("Subtraction", num_1 - num_2);
+console.log("Multiplication", num_1 * num_2);
+console.log("Division", num_1 / num_2);
+// Question 8
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+// Question 9
+var my_num = +32376770;
+console.log('My fav number ${my_num}');
+// Question 11
+var my_arr = ["Ali", "Tayyab", "Amir"];
+for (var _i = 0, my_arr_1 = my_arr; _i < my_arr_1.length; _i++) {
+    var name_1 = my_arr_1[_i];
+    console.log(name_1);
+}
